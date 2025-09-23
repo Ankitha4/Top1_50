f4d46fa17c3d36c30b6d94d4d6a93edccd2cfd3a
Ankitha4 <anki.shetty20@gmail.com>
2025-09-23 22:23:41 +0530

    commit and push 22_23_24

--- Top21_25/T_21_25.py
+++ Top21_25/T_21_25.py
@@ -30,4 +30,68 @@ for i in s1:
         break
 print("Is numeric",only_numeric)
 
+#22.Count words in a sentence
+sen = "I love python programming"
+word_list = sen.split()
+count = len(word_list)
+count1 = sum([1 for ch in word_list])
+print(count)
+print(count1)
+
+#without split
+
+in_word = False
+con = 0
+for ch in sen:
+    if ch != " " and not in_word:
+        con +=1
+        in_word = True
+    elif ch == " ":
+        in_word = False
+print(con)
+
+
+#23.Remove the specific character
+def rem_character(s,ch):
+    new_word = ''
+    for i in s:
+        if i != ch:
+            new_word += i
+    print(new_word)
+
+rem_character("ankitha","i")
+
+#24.Find the shortest word in the sentence:
+def short_word(s):
+    s_words = s.split(" ")
+    short_w = s_words[0]
+    for words in s_words:
+        if len(short_w) > len(words):
+            short_w = words
+    print(short_w)
+short_word("Find the shortest word in the sentence")
+
+def short_words(s):
+    s_words = s.split()
+    min_len = min(len(words) for words in s_words)
+    shrt_word = [words for words in s_words if len(words)== min_len]
+    print(shrt_word)
+
+short_words("I am at a bar")
+
+
+def short_words(s):
+    s_words = s.split()
+    min_len= float("inf")
+    for words in s_words:
+        if min_len > len(words):
+            min_len = len(words)
+    short_w = [words for words in s_words if len(words)== min_len]
+    print(short_w)
+
+short_words("I am at a bar")
+
+#25.Longest palindromic substring:
+
+
 
